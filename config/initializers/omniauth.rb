c25c1b694b6b4b67b679cb71409547b05e634c2e
veruca-salt <joyce.tedone@gmail.com>
2019-01-16 16:17:39 +0000

    Domain

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_AUTH_CLIENT'], ENV['GOOGLE_AUTH_SECRET']
   {
   	  prompt: 'select_account',
-      hd: 'digital.cabinet-office.gov.uk'
+      hd: ENV['DOMAIN']
   }
 end
