5a58a9351f0699d147f492008700d0aa1c8565c8
veruca-salt <joyce.tedone@gmail.com>
2019-02-07 13:12:13 +0000

    update assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( govuk-frontend-2.6.0.min.css )
+Rails.application.config.assets.precompile += %w( govuk-frontend-ie8-2.6.0.min.css )
+Rails.application.config.assets.precompile += %w( govuk-frontend-2.6.0.min.js )
